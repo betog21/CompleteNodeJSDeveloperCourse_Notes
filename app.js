2f3140c5d9838c0d09692949f3c96463e3feff3f
Alberto González <alberto.gonzalez@distillery.com>
2022-03-17 11:25:40 -0600

    New commands were added to the project.

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-const getNotes = require("./notes.js");
 const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes.js");
 
-const command = process.argv[2]
+//customize yargs version
+yargs.version("1.1.0");
 
-if(command == 'add'){
-    console.log('Adding.');
-}
-if(command == 'remove'){
-    console.log('Removing.');
-}
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding note.");
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note.");
+  },
+});
+
+// Create list command
+yargs.command({
+    command:"list",
+    describe: "List the notes",
+    handler: function(){
+        console.log("Listing the notes.");
+    }
+})
+
+// Create read command
+yargs.command({
+    command: "read",
+    describe: "Reading a note",
+    handler: function(){
+        console.log("Reading the note");
+    }
+})
+
+console.log(yargs.argv);
