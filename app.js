60afcbfca2460677c2b0b36ef68e519387083e74
Alberto González Balderas <albertoglezb@hotmail.com>
2022-03-17 07:52:03 -0600

    A merge was implemented to usage the validator and the chalk library.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const getNotes = require("./notes.js");
+const validator = require("validator");
 const chalk = require("chalk");
 
 const message = getNotes();
-
+console.log(chalk.green(validator.isURL("example@examplem")));
 console.log(chalk.blue(message));
