a1aca1887bbf587a5c47311d27abc6e73604b5d9
Alberto González <alberto.gonzalez@distillery.com>
2022-03-17 10:54:33 -0600

    The validation of the second parameter was implemented.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const getNotes = require("./notes.js");
-const validator = require("validator");
 const chalk = require("chalk");
 
-const message = getNotes();
-console.log(chalk.green(validator.isURL("example@examplem")));
-console.log(chalk.blue(message));
+const command = process.argv[2]
+
+if(command == 'add'){
+    console.log('Adding.');
+}
+if(command == 'remove'){
+    console.log('Removing.');
+}
