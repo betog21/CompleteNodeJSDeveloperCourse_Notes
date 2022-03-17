bf417be0b6ad5a6da21d6279e9fe8610ee7fab46
Alberto González <alberto.gonzalez@distillery.com>
2022-03-17 12:53:44 -0600

    The add note funtionality was implemented and the notes.json file was created.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes.js");
+const { getNotes, addNote } = require("./notes.js");
 
 //customize yargs version
 yargs.version("1.1.0");
@@ -22,7 +22,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.table([{ Title: argv.title, Body: argv.body }]);
+    addNote(argv.title, argv.body);
   },
 });
 
--- notes.js
+++ notes.js
@@ -1,5 +1,39 @@
+const fs = require("fs");
+
 const getNotes = function () {
   return "Getting notes";
 };
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const duplicatedNotes = notes.filter((note) => note.title == title);
+
+  if (duplicatedNotes.length == 0) {
+    notes.push({
+      title,
+      body,
+    });
+
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.error("Note title taken!");
+  }
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+module.exports = { getNotes, addNote };
